da528d08f1aab7674425c15c1946474ff9ab1f9b
dmitryame <dmitry@custommode.com>
2025-10-11 18:44:18 -0400

    chore: update Babel and ESLint configurations, add Prettier support, and enhance project structure

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
-module.exports = function(api) {
+/* eslint-env node */
+/* global module */
+module.exports = function babelConfig(api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      '@babel/preset-typescript'
-    ],
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript']
   };
 };
