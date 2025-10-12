c3fd3a1ecf8e21fce73f18660110b7f6077ee58b
dmitryame <dmitry@custommode.com>
2025-10-12 18:17:14 -0400

    fix: update package dependencies to specific versions and improve DOCUMENT_FOLDER initialization logic

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 
 import { Paths } from 'expo-file-system'
 
-export const DOCUMENT_FOLDER: string = Paths.document.uri || ''
+export const DOCUMENT_FOLDER: string = Paths.document.uri !== '' ? Paths.document.uri : ''
